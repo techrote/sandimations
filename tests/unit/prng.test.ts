16f7ea14e65d9370d99f60e5e82ca38e5bf32f81
tom <261801702+techrote@users.noreply.github.com>
2026-09-15 21:20:26 +0100

    style: format SD-002 PRNG test

--- tests/unit/prng.test.ts
+++ tests/unit/prng.test.ts
@@ -5,12 +5,7 @@ describe('SeededPrng', () => {
   it('matches the fixed SD-002 reference vector', () => {
     const prng = new SeededPrng(0x12345678);
     expect(Array.from({ length: 6 }, () => prng.nextUint32())).toEqual([
-      0x1b2cc72e,
-      0xf0f77b89,
-      0xf09b5c53,
-      0x3bdfdfd7,
-      0xe7930f7b,
-      0xc740a7fb,
+      0x1b2cc72e, 0xf0f77b89, 0xf09b5c53, 0x3bdfdfd7, 0xe7930f7b, 0xc740a7fb,
     ]);
   });
 
